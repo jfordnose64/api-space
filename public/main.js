4a4f2c63141037e6d9de46e6672eda0b81bbfaac
jackson ford <jfordnose64@gmail.com>
2019-08-15 14:51:03 -0400

    working image

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
+const API_URL = 'https://sdg-astro-api.herokuapp.com/api/Nasa/apod'
+const spaceX_URL = ''
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+  nasaPhoto()
+}
+
+const nasaPhoto = () => {
+  fetch(API_URL)
+    .then(response => {
+      return response.json()
+    })
+    .then(nasaPic => {
+      // console.log(hdUrl)
+      console.log(nasaPic.hdUrl)
+      // adding the url to the page
+      document.querySelector(
+        '.image-container'
+      ).style.backgroundImage = `url('${nasaPic.hdUrl}')`
+
+      console.log(nasaPic.copyright)
+      document.querySelector('.copy').textContent = `copyright: ${
+        nasaPic.copyright
+      } | title: ${nasaPic.title}`
+    })
+}
+
+const spaceX = () => {
+  fetch
 }
 
 document.addEventListener('DOMContentLoaded', main)
